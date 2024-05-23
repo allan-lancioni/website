cc7273b6c1487e1bc5ed58b725c555918f0894b5
Allan Lancioni <allanlancioni@allanlancioni.com>
2024-05-23 09:42:40 -0300

    feat: Adding Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         <main className="container mx-auto p-8 flex-grow">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
