9fd192fd8b0eef6cf144b3654467fae84aa51693
Allan Lancioni <allanlancioni@allanlancioni.com>
2024-05-23 09:54:46 -0300

    bugfix: Remove extra semicolon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col`} style={{ minHeight: '100vh' }}>
+      <body
+        className={`${inter.className} flex flex-col`}
+        style={{ minHeight: '100vh' }}
+      >
         <Header />
         <main className="container mx-auto p-8 flex-grow">{children}</main>
         <Footer />
