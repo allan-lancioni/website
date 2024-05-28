afe550b092161b70cc48d5e7790dd650cd4b41d2
Allan Lancioni <allanlancioni@allanlancioni.com>
2024-05-27 23:16:04 -0300

    bugfix: Image and postInstall

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['via.placeholder.com'],
+  },
   async redirects() {
     return [
       {
